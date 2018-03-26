542c73b469d7609c6324122c9df4272a2595bbbc
Daniel <dgading@gmail.com>
2018-03-26 15:07:21 -0400

    Add filter js file

---
+++ components/filter_types.js
@@ -0,0 +1,6 @@
+export const locationIds = [ // Full list of ~50 on section 9.2
+  {name: "William J. Clinton Library", id: 1},
+  {name: "Dwight D. Eisenhower Library", id: 2},
+  {name: "Franklin D. Roosevelt Library", id: 3},
+  {name: "National Archives at Philadelphia", id: 17},
+];
